f7d8418ea567826008d69862e46d4c4e0ea17343
Doyoung Lee <doyoung930@gmail.com>
2024-11-03 18:40:19 +0900

    Update: refactoring code

---
+++ Algorithm/Algorithm/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+// push_back O(1)
+// 중간 삽입/ 삭제 O(N)
+// 임의 접근 O(1)
+// [][][][][]
+// 동적배열
+// 
+// 
+// [] <-> [] <-> [] <-> []
+// 연결 리스트
+// 삽입/삭제 O(1)
+// 임의 접근 O(N)
+// 
+// 
+// 스택 O(1)
+// 큐 O(1)
+//
+int main()
+{
+}

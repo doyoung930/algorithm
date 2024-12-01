4bb7c77c9ef147b69ff6b7e15bf4acfc0d6aeac2
doyoung930 <doyoung930@gmail.com>
2024-12-02 04:51:42 +0900

    Algorithm: hash

---
+++ Algorithm/Algorithm/HashTable.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+// Q) map vs hash_map 차이점(unordered_map)
+//
+// map : Red-Black Tree
+// - 추가 / 탐색 / 삭제 O(logN)
+// 
+// C# dictionary = C++ unordered_map
+// 
+// hash_map(unordered_map)
+// - 추가 / 탐색 / 삭제 O(1)
+//
+
+void TestHash() {
+	struct User {
+		int userid = 0;
+
+		string username;
+	};
+
+	vector<User> users;
+	users.resize(1000);
+
+	const int userid = 123456789;
+	int key = (userid % 1000); // hash < 고유 번호
+
+	// 123456789번 유저 정보 세팅
+	users[key] = User{ 777, "aaa" };
+
+	User& user = users[key];
+	if (user.userid == userid) {
+
+		string name = users[777].username;
+		cout << name << endl;
+	}
+
+	// 충돌 문제 - key 값이 겹침
+
+}
+int main()
+{
+	TestHash();
+}
